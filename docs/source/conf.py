a4df300d25843635e09a41087c17ab6882c68615
Mina Yi <mina7928@naver.com>
2022-08-01 17:36:48 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Test'
+copyright = '2022, Mina Yi'
+author = 'Mina Yi'
 
 release = '0.1'
 version = '0.1.0'
